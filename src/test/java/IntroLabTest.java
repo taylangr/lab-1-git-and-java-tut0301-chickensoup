dd165cb388bdad12658b813d3264ea4ce8bb7d49
a-gohar <90384700+a-gohar@users.noreply.github.com>
2021-09-13 20:49:52 +0100

    Update IntroLabTest.java

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
