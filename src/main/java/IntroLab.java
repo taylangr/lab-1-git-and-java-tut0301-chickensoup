94aecbf605496ddd4e74470522bcbc51f28e63c3
Linh <linhquan.hoang@mail.utoronto.ca>
2021-09-13 15:55:42 -0400

    files2

--- src/main/java/IntroLab.java
+++ src/main/java/IntroLab.java
@@ -40,6 +40,10 @@ public class IntroLab {
      * As a reference, we've provided the sample method exampleStudent.
      * Make sure you document your method properly!
      */
+    public static String hoangli3() {
+        return printLabMessage("Ahoy", "Linh", false,
+                "I wanna take a nap...");
+    }
 
     /**
      * An example method that calls on printLabMessage.
@@ -59,6 +63,7 @@ public class IntroLab {
          * As a reference, we've provided a sample call to exampleStudent
          * above. Afterwards: run this file to see the output!
          */
+        System.out.println(hoangli3());
 
 
     }
--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -25,9 +25,17 @@ public class IntroLabTest {
         assertEquals(expected, actual);
     }
 
+
     /* TODO: Write a test case for the method you wrote in IntroLab.
      *       If done properly, you should be able to run IntroLabTest and see
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 1000)
+    public void testhoangli3() {
+        String expected = "Ahoy! My name is Linh! " +
+                "I like dogs more than cats! I wanna take a nap...";
+        String actual = IntroLab.hoangli3();
+        assertEquals(expected, actual);
+    }
 }
