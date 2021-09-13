996a7281d5f912a9752e76e902e34a780c579606
jenniefang <jennie.fang@mail.utoronto.ca>
2021-09-13 16:11:18 -0400

    IntroLabTest Jennie Fang

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -9,6 +9,7 @@ import org.junit.*;
 import static org.junit.Assert.*;
 
 public class IntroLabTest {
+
     @Before
     public void setUp() {
     }
@@ -22,6 +23,14 @@ public class IntroLabTest {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
         String actual = IntroLab.exampleStudent();
+        assertEquals(expected, actual);
+    }
+
+    @Test(timeout = 10)
+    public void testfangjenn() {
+        String expected = "Hi! My name is Jennie Fang! " +
+                "I like dogs more than cats!" + " I am a student at UofT." ;
+        String actual = IntroLab.fangjenn();
         assertEquals(expected, actual);
     }
 
