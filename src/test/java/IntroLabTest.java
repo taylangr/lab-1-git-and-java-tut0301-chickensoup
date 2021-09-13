c59206585d1b36eb7fff03ee151e8e5d52ed9fa2
noaoch <jerry.yan@mail.utoronto.ca>
2021-09-13 15:59:21 -0400

    Added yanjerry to IntroLab and relevant test in IntroLabTest

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 1000)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,13 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+
+    @Test(timeout = 1000)
+    public void testYanJerry() {
+        String expected = "Hi! My name is Jerry! " +
+                "I like dogs more than cats! I love yogurt.";
+        String actual = IntroLab.yanJerry();
+        assertEquals(expected, actual);
+    }
 }
+
